c132baac8eed3c2609ba69ca0baf204aaeae9a25
afverschuur <fred.verschuur@gmail.com>
2025-02-11 17:59:06 +0100

    Ship left-right control and ship speed

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ class Settings:
     """ Class with all settings for Alien Invasion """
 
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 900
+        self.screen_height = 600
         self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
     
--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
 
     def __init__(self, ai_game) -> None:
         """ Init ship and set starting position """
+        self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
@@ -14,15 +15,24 @@ class Ship:
         
         # Start at bottom center
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
         # Movement flags
         self.moving_right = False
+        self.moving_left = False
     
     def update(self):
         """ Update position based on movement flags """
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             # Move ship to the right
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            # Move ship to the left
+            self.x -= self.settings.ship_speed
+        
+        # Update ship's rect from self.x
+        self.rect.x = self.x
+
 
     def blitme(self):
         """ Draw ship at current position """
